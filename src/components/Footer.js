4f192a7c899fdb7dd20d1e8bd9e332aef58279d6
Ranmini <ranminierandika3@gmail.com>
2023-03-20 18:59:10 +0530

    first commit (started)

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='b-0 col-lg-12 bg-dark p-2 text-center'>
+        <span className='text-light'>Copyright @Ranmini Erandika - Faculty of Science - University of Peradeniya</span>
+    </div>
+  )
+}
